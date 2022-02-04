e30e31219650531c98213ed064b20b1386e2e968
Paul Cooper <paul_cooper_pc@hotmail.com>
2022-02-04 15:44:36 +0000

    Notes.js file refactored

--- src/notes.js
+++ src/notes.js
@@ -1,12 +1,23 @@
-class Notes {
+class Library {
     constructor() {
         this.storageObj = [];
     }
 
     add(title, content) {
-        let element = {};
-        element.title = title;
-        element.content = content;
-        this.storageObj.push(element);
+        this.storageObj.push({ title: title, content: content });
+        let storageJson = JSON.stringify(this.storageObj);
+        localStorage.setItem("My_Notes", storageJson);
+    }
+
+    view() {
+        this.storageObj = JSON.parse(localStorage.getItem("My_Notes"));
+        return this.storageObj;
+    }
+}
+
+class Notes {
+    constructor(title, content) {
+        this.title = title;
+        this.content = content;
     }
 }
